5dd951918e5d54acfa6a1bc3656cefac0eefaa9a
Tanya S <tanlull@gmail.com>
2021-01-21 17:17:04 +0700

    mongoose

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,9 @@ const express = require("express");
 const { route } = require(".");
 const router = express.Router();
 
+const Player = require('../models/Player')
+const Team = require('../models/Team')
+
 const players = [
 	{firstName:"eli", lastName:"manning", position:"qb", age:37, team:"nyg"},
 	{firstName:"tom", lastName:"brady", position:"qb", age:41, team:"nep"},
@@ -19,6 +22,22 @@ const db = {
   team: teams,
   player: players,
 };
+
+router.get('/team',(req,res)=>{
+  Team.find(null)
+  .then(data =>{
+    res.json({
+      confirmation:'Success',
+      message:data
+    })
+  })
+  .catch(err =>{
+    res.json({
+      confirmation:'fail',
+      message:err.message
+    })
+  })
+})
 
 router.get("/:resource", (req, res) => {
   const resource = req.params.resource;
